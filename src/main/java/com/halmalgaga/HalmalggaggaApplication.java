c2ce61234d87083b4c423f608e45019166310498
강민희 <himinhee1113@gmail.com>
2023-05-08 00:48:45 +0900

    create halmalggagga space

---
+++ src/main/java/com/halmalgaga/HalmalggaggaApplication.java
@@ -0,0 +1,13 @@
+package com.halmalgaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalmalggaggaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalmalggaggaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halmalgaga/HalmalggaggaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halmalgaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalmalggaggaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
