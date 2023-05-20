f8605d03d84923498377e360248d305319621fac
green <himinhee1113@gmail.com>
2023-05-20 23:25:19 +0900

    [테스트 API] hello

--- src/main/java/com/halmalgaga/HalmalggaggaApplication.java
+++ src/main/java/com/halmalgaga/HalmalggaggaApplication.java
@@ -2,12 +2,21 @@ package com.halmalgaga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HalmalggaggaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HalmalggaggaApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "wetaggu") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
