949d6e90cf8efc35e5711913b4357c924971caf2
박유정 <dbwjd1154@gmail.com>
2023-07-17 12:26:02 +0900

    connection test

--- src/main/java/com/halmalgaga/HalmalggaggaApplication.java
+++ src/main/java/com/halmalgaga/HalmalggaggaApplication.java
@@ -18,5 +18,7 @@ public class HalmalggaggaApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "wetaggu") String name) {
 		return String.format("Hello %s!", name);
 	}
+	
+	// yoozung commit test
 
 }
